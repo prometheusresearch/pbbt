63da9df3bb6adf6a52fa1989bcb7dac68799d11c
alexvoronoi <alex@prometheusresearch.com>
2026-04-15 15:48:43 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import sys
 
 
 NAME = "pbbt"
-VERSION = "0.2.0"
+VERSION = "0.2.1"
 DESCRIPTION = """Pluggable Black-Box Testing toolkit"""
 LONG_DESCRIPTION = open('README', 'r').read()
 AUTHOR = """Kirill Simonov (Prometheus Research, LLC)"""
